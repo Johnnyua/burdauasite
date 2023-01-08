573c876c382c45a33003813f32e84ad0fe854504
Home <y.dyachenko@gmail.com>
2023-01-08 14:46:03 +0200

    change header

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 } else {
     body.classList.remove("mobile");
 }
+
+window.addEventListener("load", init);
+
+function init() {
+    burger.addEventListener("click", showMobileMenu);
+    search.addEventListener("click", showSearchInput);
+}
+
+function showMobileMenu(e) {
+  const mobileNav = document.querySelector(".header__nav");
+  mobileNav.classList.toggle("open");
+  burger.classList.toggle("burger-close");
+  e.currentTarget.children[0].classList.toggle("burger-rotate-positive");
+  e.currentTarget.children[1].classList.toggle("display-none");
+  e.currentTarget.children[2].classList.toggle("burger-rotate-negative");
+}
+
+function showSearchInput(e) {
+    const searchInput = document.querySelector(".search_input");
+    searchInput.classList.toggle("active");
+}
