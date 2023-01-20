e87ef5c8e494ec53c59f163c7a6664939e14bd13
Johnnyua <ev.dyachenko@gmail.com>
2023-01-20 16:22:41 +0200

    finish header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-const body = document.body;
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    body.classList.add("mobile");
-} else {
-    body.classList.remove("mobile");
-}
+// const body = document.body;
+// if (
+//   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//     navigator.userAgent
+//   )
+// ) {
+//   body.classList.add("mobile");
+// } else {
+//   body.classList.remove("mobile");
+// }
 
 window.addEventListener("load", init);
+const langLink = document.querySelectorAll(".lang__list .lang__link");
 
 function init() {
-    burger.addEventListener("click", showMobileMenu);
+  burger.addEventListener("click", showMobileMenu);
     search.addEventListener("click", showSearchInput);
+    langLink.forEach((elem) => {
+        elem.addEventListener("click", changeLanguage);;
+    });
 }
 
 function showMobileMenu(e) {
@@ -22,6 +30,19 @@ function showMobileMenu(e) {
 }
 
 function showSearchInput(e) {
-    const searchInput = document.querySelector(".search_input");
-    searchInput.classList.toggle("active");
+  const searchInput = document.querySelector(".search_input");
+  searchInput.classList.toggle("active");
+  searchInput.focus();
+  searchInput.onblur = function () {
+    this.value = "";
+    this.classList.remove("active");
+  };
+}
+
+function changeLanguage(e) {
+  const activeLang = document.querySelector(".lang__link.active");
+  if (!e.target.classList.contains("active")) {
+    activeLang.classList.remove("active");
+    e.target.classList.add("active");
+  }
 }
