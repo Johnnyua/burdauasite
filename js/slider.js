834d51284f0f9879cb931146cfcad8f5b26b4fc4
Home <y.dyachenko@gmail.com>
2023-01-31 19:38:12 +0200

    add site section

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
-new Swiper(".projects__cards_container", {
-  slidesPerView: 3,
-  grid: {
-      rows: 1,
-      column: 3,
-  },
-});
+// new Swiper(".projects__cards_container", {
+//   slidesPerView: 3,
+//   grid: {
+//       rows: 1,
+//       column: 3,
+//   },
+// });
