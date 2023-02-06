b3accfeefb4a3efc43d01a7e67e30133609f2476
Johnnyua <ev.dyachenko@gmail.com>
2023-02-06 12:42:53 +0200

    add reviews swiper slider

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,28 @@
-// new Swiper(".projects__cards_container", {
-//   slidesPerView: 3,
-//   grid: {
-//       rows: 1,
-//       column: 3,
-//   },
-// });
+new Swiper(".reviews-swiper-container", {
+  slidesPerView: 3,
+  grid: {
+    rows: 1,
+  },
+  pagination: {
+    el: ".reviews__cards_pagination",
+    type: "bullets",
+    clickable: true,
+    // dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".reviews__cards_button-next",
+    prevEl: ".reviews__cards_button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      centeredSlides: true,
+    },
+    701: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
