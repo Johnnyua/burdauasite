bde9f457175a47ae35464e5407ce21ef7c5fa648
Johnnyua <ev.dyachenko@gmail.com>
2023-02-09 18:11:03 +0200

    add preview projects slider

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,4 @@
-new Swiper(".reviews-swiper-container",
-  {
+new Swiper(".reviews-swiper-container", {
   slidesPerView: 3,
   grid: {
     rows: 1,
@@ -28,6 +27,39 @@ new Swiper(".reviews-swiper-container",
   },
 });
 
-new Swiper(".projects__slides_swiper-conrainer", {
-  slidesPerView: 5,
+new Swiper(".projects__slides_container", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  initialSlide: 2,
+  loop: true,
+  slideToClickedSlide: true,
+  loop: true,
+  navigation: {
+    nextEl: ".projects__slides_button-next",
+    prevEl: ".projects__slides_button-prev",
+  },
+  thumbs: {
+    swiper: {
+      el: ".projects__slides__preview_container",
+      centeredSlides: true,
+      initialSlide: 2,
+      loop: true,
+      slideToClickedSlide: true,
+      breakpoints: {
+        320: {
+          slidesPerView: 3,
+        },
+        701: {
+          slidesPerView: 4.5,
+        },
+        991: {
+          slidesPerView: 5.5,
+        },
+      },
+      navigation: {
+        nextEl: ".projects__slides_button-next",
+        prevEl: ".projects__slides_button-prev",
+      },
+    },
+  },
 });
