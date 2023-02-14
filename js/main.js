67643bbd3d30a51e0d9b44d15bc9d964c931fd9e
Home <y.dyachenko@gmail.com>
2023-02-14 19:11:41 +0200

    add service digital page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-
-
 window.addEventListener("load", init);
-const langLink = document.querySelectorAll(".lang__list .lang__link");
 
 function init() {
+  const langLink = document.querySelectorAll(".lang__list .lang__link");
   burger.addEventListener("click", showMobileMenu);
-    searchIcon.addEventListener("click", showSearch);
-    langLink.forEach((elem) => {
-        elem.addEventListener("click", changeLanguage);;
-    });
+  searchIcon.addEventListener("click", showSearch);
+  langLink.forEach((elem) => {
+    elem.addEventListener("click", changeLanguage);
+  });
 }
 
-function menuActivate() {
-  const nav = document.querySelectorAll("");
-}
 
 function showMobileMenu(e) {
   const mobileNav = document.querySelector(".header__menu");
@@ -25,10 +20,12 @@ function showMobileMenu(e) {
 }
 
 function showSearch(e) {
+  e.stopPropagation();
   const searchText = document.querySelector(".search__text");
   const searchInput = document.querySelector(".search__input");
+
   searchText.classList.toggle("active");
-  searchInput.focus();
+  setTimeout(() => searchInput.focus(), 30);
   searchInput.onblur = function () {
     this.value = "";
     searchText.classList.remove("active");
@@ -50,35 +47,39 @@ function changeLanguage(e) {
 //   },
 // );
 
-$("body").click(
-  function() {
-    $(".header__menu-services").removeClass("active");
-  },
-);
+$("body").click(function (e) {
+  $(".header__menu-services").removeClass("active");
+});
 
-const reviewItemList = document.querySelectorAll(
-  ".reviews__item"
-);
+const reviewItemList = document.querySelectorAll(".reviews__item");
 const reviewItemCountClassName = ".cards__text p";
 const reviewItemBtnClassName = ".cards__btn";
 
-window.addEventListener("resize", function() {showContainerElementByClassName(
-  reviewItemList,
-  reviewItemCountClassName,
-  reviewItemBtnClassName
-);})
+window.addEventListener("resize", function () {
+  showContainerElementByClassName(
+    reviewItemList,
+    reviewItemCountClassName,
+    reviewItemBtnClassName
+  );
+});
 
-function showContainerElementByClassName(containerList, classNameCount, classNameShow) {
+function showContainerElementByClassName(
+  containerList,
+  classNameCount,
+  classNameShow
+) {
   const rowCount = 6;
   for (i = 0; i < containerList.length; i++) {
     const containerItem = containerList[i].querySelector(classNameCount);
     const currentRowCount = computeRowContainer(containerItem);
-    const showEl = containerList[i].querySelector(classNameShow); 
+    const showEl = containerList[i].querySelector(classNameShow);
     if (currentRowCount > rowCount) {
       showEl.classList.add("show");
-      showEl.addEventListener("click", function (e) { showMoreText(e, containerItem, showEl); });
+      showEl.addEventListener("click", function (e) {
+        showMoreText(e, containerItem, showEl);
+      });
     } else {
-      showEl.classList.remove("show");  
+      showEl.classList.remove("show");
     }
   }
 }
@@ -89,16 +90,15 @@ function showMoreText(e, containerItem, showEl) {
 }
 
 function computeRowContainer(container) {
-    
-    const style = window.getComputedStyle(container);
-    const containerHeight = parseFloat(style.height);
-    const lineHeight = parseFloat(style.lineHeight);
+  const style = window.getComputedStyle(container);
+  const containerHeight = parseFloat(style.height);
+  const lineHeight = parseFloat(style.lineHeight);
 
-    const currentRowsCount = Math.ceil(containerHeight / lineHeight);
-  
-    return currentRowsCount;
+  const currentRowsCount = Math.ceil(containerHeight / lineHeight);
+
+  return currentRowsCount;
 }
-   
+
 showContainerElementByClassName(
   reviewItemList,
   reviewItemCountClassName,
